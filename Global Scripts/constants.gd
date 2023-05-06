0fe5ed32c736648a1d68784bc8bd066178a20a73
Shea Frembling <sfrembling@gmail.com>
2023-05-06 13:59:43 -0500

    Update game title

--- Global Scripts/constants.gd
+++ Global Scripts/constants.gd
@@ -1,7 +1,7 @@
 extends Node
 
 const GAME_VERSION = "0.1.1"
-const GAME_TITLE = "GoGodot Game Jam Project"
+const GAME_TITLE = "Cosmos"
 
 const SCREEN_PLAY = "screen_play"
 const SCREEN_ABOUT = "screen_about"
